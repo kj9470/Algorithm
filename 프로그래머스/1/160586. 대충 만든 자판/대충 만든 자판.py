bf4e5dbe0d21d98fe16676da7be4c08cb7e900c9
KwonJeong <48792069+kj9470@users.noreply.github.com>
2024-08-14 17:11:03 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.51 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,25 @@
+def solution(keymap, targets):
+    answer = []
+    dic = {}
+    
+    key = "".join(targets)
+    key = list(set(key))
+    dic = dict.fromkeys(key, 999)
+
+    for map in keymap:
+        for i in range(len(map)):
+            m = map[i]
+            if (m in dic and dic[m] > i + 1):
+                dic[m] = i + 1
+
+    for tar in targets:
+        tmp = 0
+        for t in tar:
+            v = dic[t]
+            if v == 999:
+                tmp = -1
+                break
+            tmp += int(dic[t])
+        answer.append(tmp)
+
+    return answer
