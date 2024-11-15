aa8bde228c45cda5edd98e065316863671848478
KwonJeong <48792069+kj9470@users.noreply.github.com>
2024-11-15 23:00:33 +0900

    [Bronze I] Title: 색종이, Time: 532 ms, Memory: 118292 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -3,9 +3,13 @@ arr = [[ 0 for _ in range(1001) ] for _ in range(1001) ]
 
 for n in range(N):
     x, y, width, height = map(int, input().split())
-    for i in range(x, x + width, 1):
-        for j in range(y, y + height, 1):
+    for i in range(x, x + width):
+        for j in range(y, y + height):
             arr[i][j] = n + 1
 
+count = 0
 for i in range(N):
-    print(sum(row.count(i + 1) for row in arr))
+    for j in range(len(arr)):
+        count += arr[j].count(i + 1)
+    print(count)
+    count = 0
