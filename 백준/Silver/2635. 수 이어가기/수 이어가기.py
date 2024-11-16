d6c5bc99d0e7e15912dbf9f68a0ac4fab91a79f3
KwonJeong <48792069+kj9470@users.noreply.github.com>
2024-11-16 17:34:15 +0900

    [Silver V] Title: 수 이어가기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+result = [N]
+for i in range(1, N + 1):
+    tmp = [N, i]
+    while tmp[-1] >= 0:
+        tmp.append(tmp[-2] - tmp[-1])
+    tmp.pop()
+    if len(tmp) > len(result):
+        result = tmp
+
+print(len(result))
+print(*result)
