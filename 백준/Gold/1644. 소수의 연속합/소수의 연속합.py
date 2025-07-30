f68ba2f13d03bfadcf0e65eedd5f905dcbfaf417
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-07-30 17:13:38 +0900

    [Gold III] Title: 소수의 연속합, Time: 1192 ms, Memory: 227832 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -1,22 +1,22 @@
 import sys
-import math
 
 input = sys.stdin.readline
 N = int(input())
-prime_list = []
 end = 0
 count = 0
 sum = 0
 
 def is_prime(N):
-    for i in range(2, int(math.sqrt(N)) + 1):
-        if N % i == 0:
-            return False
-    return True
+    prime_list = [i for i in range(N + 1)]
+    end = int(N**(1/2))
+    for i in range(2, end + 1):
+        if prime_list[i] == 0:
+            continue
+        for j in range(i * i, N + 1, i):
+            prime_list[j] = 0
+    return [i for i in prime_list[2:] if prime_list[i]]
 
-for i in range(2, N + 1):
-    if is_prime(i) == True:
-        prime_list.append(i)
+prime_list = is_prime(N)
 
 for start in range(len(prime_list)):
     while end < len(prime_list) and sum < N:
