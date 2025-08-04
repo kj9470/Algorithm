47e392be5e5e43bb37fbdcb3ad94436faef54571
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-08-04 18:59:56 +0900

    [Silver I] Title: 영역 구하기, Time: 64 ms, Memory: 34992 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+M, N, K = map(int, input().split())
+
+paper = [[0] * M for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
+count = 0
+result = []
+
+for _ in range(K):
+    x, y, nx, ny = map(int, input().split())
+    for i in range(x, nx):
+        for j in range(y, ny):
+            paper[i][j] = -1
+
+
+def bfs(x, y, visited):
+    queue = deque([(x, y)])
+    visited[x][y] = True
+    move = [[1, 0], [-1, 0], [0, -1], [0, 1]]
+    return_count = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for dx, dy in move:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M:
+                if not visited[nx][ny] and not paper[nx][ny] == -1:
+                    queue.append((nx, ny))
+                    visited[nx][ny] = True
+                    return_count += 1
+    return return_count
+
+for i in range(N):
+    for j in range(M):
+        if not paper[i][j] == -1 and not visited[i][j]:
+            result.append(bfs(i, j, visited))
+            count += 1
+
+result.sort()
+print(count)
+print(*result)
