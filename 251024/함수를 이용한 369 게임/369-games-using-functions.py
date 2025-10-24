d9615d6af1250ee3d9e5ffc76cc4787f71cdfc34
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-10-24 13:44:52 +0900

    Title: 함수를 이용한 369 게임, Time: 71ms, Memory: 17MB, Status: Passed - Codetree

--- 251024/함수를 이용한 369 게임/369-games-using-functions.py
+++ 251024/함수를 이용한 369 게임/369-games-using-functions.py
@@ -3,9 +3,7 @@ a, b = map(int, input().split())
 def num(a, b):
     count = 0
     for i in range(a, b + 1):
-        if '3' in str(i) or '6' in str(i) or '9' in str(i):
-            count += 1
-        elif i % 3 == 0:
+        if '3' in str(i) or '6' in str(i) or '9' in str(i) or i % 3 == 0:
             count += 1
     return count
 
