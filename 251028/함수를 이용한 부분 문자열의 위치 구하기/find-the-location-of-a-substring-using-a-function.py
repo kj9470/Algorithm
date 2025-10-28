78324bfc23ff11293f7ec73622c83c4c3549fe35
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-10-28 09:02:11 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251028/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 251028/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -2,10 +2,6 @@ text = input()
 pattern = input()
 
 def find(test, pattern):
-    index = 0
-    for i in range(len(text)):
-        if text[i:len(pattern) + i] == pattern:
-            return i
-    return -1
+    return text.find(pattern)
 
 print(find(text, pattern))
