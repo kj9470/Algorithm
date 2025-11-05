960821ef034d913c21e62cf24260d0c4711e2490
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-11-05 09:11:19 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251105/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
+++ 251105/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
@@ -1,11 +1,8 @@
-import sys
-sys.setrecursionlimit(3000)
-
 N = int(input())
 
 def recursion(a, b, N):
     if N == 1:
-        return b
+        return a
     return recursion(b, a * b % 100, N - 1)
 
-print(recursion(2, 4, N - 1))
+print(recursion(2, 4, N))
