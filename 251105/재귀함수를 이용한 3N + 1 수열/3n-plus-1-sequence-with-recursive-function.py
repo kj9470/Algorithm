970d73350f0f36f9f420fc97d4907c7ac4e6c73f
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-11-05 09:03:04 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251105/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.py
+++ 251105/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.py
@@ -2,8 +2,11 @@ n = int(input())
 
 def recursion(N, count):
     if N == 1:
-        return count
+        print(count)
+        return
     if N % 2 == 0:
         recursion(N // 2, count + 1)
     else:
         recursion(N * 3 + 1, count + 1)
+
+recursion(n, 0)
