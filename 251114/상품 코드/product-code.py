f4fa0f928597650e092694e3dfcb6c22b7a27732
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-11-14 18:15:50 +0900

    Title: 상품 코드, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251114/상품 코드/product-code.py
@@ -0,0 +1,13 @@
+product_name, product_code = input().split()
+product_code = int(product_code)
+
+class Product:
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+    
+product1 = Product('codetree', 50)
+product2 = Product(product_name, product_code)
+
+print(f'product {product1.code} is {product1.name}')
+print(f'product {product2.code} is {product2.name}')
