bda255c3f33e1858cfad97d4758f8a9f814dd5f9
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-11-15 16:24:58 +0900

    Title: 총점 비교, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251115/총점 비교/compare-total-points.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, name, s1, s2, s3):
+        self.name = name
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+
+n = int(input())
+students = []
+
+for _ in range(n):
+    name, s1, s2, s3 = map(str, input().split())
+    students.append(Student(name, int(s1), int(s2), int(s3)))
+
+students.sort(key=lambda x: (x.s1 + x.s2 + x.s3))
+
+for student in students:
+    print(student.name, student.s1, student.s2, student.s3)
+
