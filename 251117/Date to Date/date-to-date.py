900fdb33cc069381f27a427cd53c0c920ead3114
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-11-17 21:59:33 +0900

    Title: Date to Date, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251117/Date to Date/date-to-date.py
+++ 251117/Date to Date/date-to-date.py
@@ -1,18 +1,16 @@
 m1, d1, m2, d2 = map(int, input().split())
 
 month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-result = 0
+result = 1
 month_idx = m1
 
 while True:
     if m1 == m2 and d1 == d2:
         break
     
-    if d1 > month_days[month_idx]:
+    if d1 > month_days[m1]:
         m1 += 1
         d1 = 1
-        result += 1
-        month_idx += 1
     
     d1 += 1
     result += 1
