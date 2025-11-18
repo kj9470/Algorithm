bd72175dddb2c4927a5d886c1ee76e3d3d93b5af
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-11-19 08:19:20 +0900

    Title: 여러가지 진수변환, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251119/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,13 @@
+N, B = map(int, input().split())
+
+digits = []
+while True:
+    if N < B:
+        digits.append(N)
+        break
+    
+    digits.append(N % B)
+    N //= B
+
+for d in digits[::-1]:
+    print(d, end='')
