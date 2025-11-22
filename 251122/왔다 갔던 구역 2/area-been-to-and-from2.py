3c643cd230438f865d8cdf2b58ba3d3ac437ac27
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-11-22 14:43:12 +0900

    Title: 왔다 갔던 구역 2, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 251122/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 251122/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -5,17 +5,17 @@ for _ in range(n):
     xi, di = input().split()
     dir.append((int(xi), di))
 
-check = [0 for _ in range(200)]
+check = [0 for _ in range(2000)]
 
-idx = 0
+idx = 1000
 for x, d in dir:
     if d == 'L':
         for i in range(x):
-            check[idx + 100] += 1
             idx -= 1
+            check[idx] += 1
     if d == 'R':
         for i in range(x):
-            check[idx + 100] += 1
+            check[idx] += 1
             idx += 1
 
 count = 0
