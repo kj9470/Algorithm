c7856b9ff164eb9c4118ba8ab2fff96e9c935b7e
KwonJeong <48792069+kj9470@users.noreply.github.com>
2025-11-25 23:46:15 +0900

    Title: 사각형의 총 넓이 2, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 251125/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 251125/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,11 +1,11 @@
 n = int(input())
 OFFSET = 100
-arr = [[0] * (2 * OFFSET + 1) for _ in range(OFFSET + 1)]
+arr = [[0] * (2 * OFFSET + 1) for _ in range(2 * OFFSET + 1)]
 
 for _ in range(n):
     a, b, c, d = map(int, input().split())
-    for i in range(a, c):
-        for j in range(b, d):
+    for i in range(a + OFFSET, c + OFFSET):
+        for j in range(b + OFFSET, d + OFFSET):
             arr[i][j] = 1
 
 count = 0
