d01d986b6625ff2b2c14fd0e750bccf831a1b9f9
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-01-29 22:17:55 +0900

    [D3] Title: 두 수의 덧셈, Time: 111 ms, Memory: 27,392 KB -BaekjoonHub

--- SWEA/D3/3260. 두 수의 덧셈/두 수의 덧셈.java
+++ SWEA/D3/3260. 두 수의 덧셈/두 수의 덧셈.java
@@ -1,22 +1,46 @@
 import java.io.*;
 import java.util.*;
-import java.math.*;
 
 public class Solution {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-	
-		st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-	
-		for (int t = 1; t <= T; t++) {
-			st = new StringTokenizer(br.readLine());
-			BigInteger A = new BigInteger(st.nextToken());
-			BigInteger B = new BigInteger(st.nextToken());
-			StringBuilder sb = new StringBuilder();
-			sb.append("#").append(t).append(" ").append(A.add(B));
-			System.out.println(sb);
-		}	
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            Deque<Character> deqA = new ArrayDeque<>();
+            Deque<Character> deqB = new ArrayDeque<>();
+            Deque<Character> result = new ArrayDeque<>();
+
+            String A = st.nextToken();
+            String B = st.nextToken();
+
+            for(char ch: A.toCharArray()) {
+                deqA.push(ch);
+            }
+            for(char ch: B.toCharArray()) {
+                deqB.push(ch);
+            }
+
+            int carry = 0;
+            while (!deqA.isEmpty() || !deqB.isEmpty() || carry != 0) {
+                int a = 0, b = 0;
+                if(!deqA.isEmpty()) a = deqA.pop() - '0';
+                if(!deqB.isEmpty()) b = deqB.pop() - '0';
+
+                int sum = a + b + carry;
+                result.addFirst((char)((sum % 10) + '0'));
+                carry = sum / 10;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("#").append(t).append(" ");
+            for (char ch: result) sb.append(ch);
+
+            System.out.println(sb);
+        }
+    }
 }
