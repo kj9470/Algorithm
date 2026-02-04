13633ba0e07524993191d8f61da6332a9353a7de
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-02-04 23:24:04 +0900

    [Silver IV] Title: 큐, Time: 260 ms, Memory: 19284 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int MAX_N = 10000;
+    static int[] arr = new int[MAX_N];
+    static int front = 0;
+    static int rear = 0;
+
+    static int size() {
+        return rear - front;
+    }
+
+    static void push(int n) {
+        arr[rear++] = n;
+    }
+
+    static int pop() {
+        return arr[front++];
+    }
+
+    static boolean empty() {
+        if (size() <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+//            System.out.println(" " + front + " " + rear);
+//            System.out.println(Arrays.toString(arr));
+            st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            if (s.equals("push")) {
+                int n = Integer.parseInt(st.nextToken());
+                push(n);
+            } else if (s.equals("pop")) {
+                if ((empty())) System.out.println(-1);
+                else System.out.println(pop());
+            } else if (s.equals("size")) System.out.println(size());
+            else if (s.equals("empty")) {
+                if ((empty())) System.out.println(1);
+                else System.out.println(0);
+            } else if (s.equals("front")) {
+                if ((empty())) System.out.println(-1);
+                else System.out.println(arr[front]);
+            } else if (s.equals("back")) {
+                if ((empty())) System.out.println(-1);
+                else System.out.println(arr[rear - 1]);
+            }
+        }
+    }
+}
