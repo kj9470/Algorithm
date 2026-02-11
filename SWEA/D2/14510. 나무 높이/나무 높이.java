0d6ac842d90daf3229d865e43356d327c914e934
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-02-11 11:33:14 +0900

    [D2] Title: 나무 높이, Time: 98 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+	
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
