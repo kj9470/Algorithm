bf3137bc479b2e491099c0d55fb38b37c3f83759
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-04 00:56:00 +0900

    Title: DateTime to DateTime, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260304/DateTime to DateTime/datetime-to-datetime.js
+++ 260304/DateTime to DateTime/datetime-to-datetime.js
@@ -5,7 +5,7 @@ const [A, B, C] = input[0].split(' ').map(Number);
 
 const diff = ((A - 11) * 24 * 60 + (B - 11) * 60 + (C - 11));
 
-if (diff <= 0) {
+if (diff < 0) {
     console.log(-1);
 } else {
     console.log(diff);
