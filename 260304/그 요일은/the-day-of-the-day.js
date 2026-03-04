1a5183ebc0a18330a16afd0f87ca2e9950f7c7e1
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-04 16:07:45 +0900

    Title: 그 요일은, Time: 64ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260304/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// const dates = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+const A = input[1];
+
+const calculateDays = (m, d) => {
+    let totalDays = 0;
+
+    for (let i = 1; i < m; i++) {
+        totalDays += days[i];
+    }
+
+    totalDays += d;
+
+    return totalDays;
+}
+
+const totalDays1 = calculateDays(m1, d1);
+const totalDays2 = calculateDays(m2, d2);
+
+const diff = totalDays2 - totalDays1;
+console.log(Math.ceil(diff/7))
