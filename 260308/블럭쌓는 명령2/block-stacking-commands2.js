ca4125996f7a9ca04ebf943341c9b78c05cd3121
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-08 21:38:56 +0900

    Title: 블럭쌓는 명령2, Time: 74ms, Memory: 15MB, Status: Passed - Codetree

--- 260308/블럭쌓는 명령2/block-stacking-commands2.js
+++ 260308/블럭쌓는 명령2/block-stacking-commands2.js
@@ -3,11 +3,11 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].split(' ').map(Number);
 const segments = input.slice(1, k + 1).map(line => line.split(' ').map(Number));
 
-let seq = Array(n).fill(0);
+let seq = Array(n + 1).fill(0);
 
 for ([start, end] of segments) {
     for (let i = start; i <= end; i++) {
-        seq[i - 1] += 1;
+        seq[i] += 1;
     }
 }
 
