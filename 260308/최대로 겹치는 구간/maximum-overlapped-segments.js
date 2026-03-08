7ce516b940289ca3d22b8ffd6d78abd3452625b2
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-08 22:13:47 +0900

    Title: 최대로 겹치는 구간, Time: 70ms, Memory: 10MB, Status: Passed - Codetree

--- 260308/최대로 겹치는 구간/maximum-overlapped-segments.js
+++ 260308/최대로 겹치는 구간/maximum-overlapped-segments.js
@@ -1,3 +1,6 @@
+const MAX_SIZE = 200;
+const OFFSET = 100;
+
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
@@ -7,8 +10,10 @@ for (let i = 1; i <= n; i++) {
     segments.push(input[i].split(' ').map(Number));
 }
 
-let straightLine = Array(101).fill(0);
+let straightLine = Array(MAX_SIZE + 1).fill(0);
 for (let [start, end] of segments) {
+    start += OFFSET; // 음수 인덱스 방지
+    end += OFFSET;
     for (let i = start; i < end; i++) {
         straightLine[i] += 1;
     }
