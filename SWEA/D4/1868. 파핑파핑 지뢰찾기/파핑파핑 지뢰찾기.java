76ce29405014a075ef3d61732b5a27a5bd930df8
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-09 12:02:50 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 218 ms, Memory: 39,496 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -46,6 +46,13 @@ public class Solution {
                 }
             }
         }
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < N; j++) {
+//                System.out.print(map[i][j]);
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
     }
 
     static int checkNum() {
