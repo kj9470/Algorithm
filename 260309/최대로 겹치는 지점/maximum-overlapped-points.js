4ebc424f5f6e41b457bdf2096e47a1d6d8de9c15
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-09 17:26:36 +0900

    Title: 최대로 겹치는 지점, Time: 93ms, Memory: 10MB, Status: Passed - Codetree

--- 260309/최대로 겹치는 지점/maximum-overlapped-points.js
+++ 260309/최대로 겹치는 지점/maximum-overlapped-points.js
@@ -7,10 +7,10 @@ for (let i = 1; i <= n; i++) {
   segments.push(input[i].split(' ').map(Number));
 }
 
-
 let answer = Array(101).fill(0);
+
 for (let [x1, x2] of segments) {
-  for (let i = x1; i < x2; i++) {
+  for (let i = x1; i <= x2; i++) {
     answer[i] += 1;
   }
 }
