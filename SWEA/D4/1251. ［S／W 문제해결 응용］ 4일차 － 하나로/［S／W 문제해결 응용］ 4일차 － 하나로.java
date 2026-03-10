f90cb75ac5dafa8e1b59e8aafd3be472b19cd29e
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-10 14:06:24 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 221 ms, Memory: 63,736 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -3,16 +3,15 @@ import java.util.*;
 
 public class Solution {
     static int N;
-    static int[] xIsland, yIsland;
+    static int[] x;
+    static int[] y;
     static double E;
-    static boolean[] v;
-    static double[] P;
 
-    static double environmentalFee(int x1, int y1, int x2, int y2) {
-        int dx = x1 - x2;
-        int dy = y1 - y2;
-        double distance = (double) dx * dx + (double) dy * dy;
-        return E * distance;
+    static double cost(int x1, int y1, int x2, int y2) {
+        double x = x1 - x2;
+        double y = y1 - y2;
+        double d = (x * x) + (y * y);
+        return d * E;
     }
 
     public static void main(String[] args) throws Exception {
@@ -22,38 +21,38 @@ public class Solution {
         int T = Integer.parseInt(br.readLine());
         for (int t  = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
-            xIsland = new int[N];
-            yIsland = new int[N];
-            v = new boolean[N];
+            x = new int[N]; y = new int[N];
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
-                xIsland[i] = Integer.parseInt(st.nextToken());
+                x[i] = Integer.parseInt(st.nextToken());
             }
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                yIsland[j] = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < N; i++) {
+                y[i] = Integer.parseInt(st.nextToken());
             }
             E = Double.parseDouble(br.readLine());
 
-            P = new double[N]; Arrays.fill(P, Double.MAX_VALUE);
-            Double mst = 0.0;
+            boolean[] v = new boolean[N];
+            double[] P = new double[N]; Arrays.fill(P, Double.MAX_VALUE);
+            P[0] = 0.0;
             int cnt = 0;
-            P[0] = 0;
+            Double mst = 0.0;
             for (int i = 0; i < N; i++) {
                 int minVertex = -1;
-                Double minCost = Double.MAX_VALUE;
+                Double min = Double.MAX_VALUE;
                 for (int j = 0; j < N; j++) {
-                    if (!v[j] && minCost > P[j]) {
-                        minCost = P[j]; minVertex = j;
+                    if (!v[j] && min > P[j]) {
+                        min = P[j];
+                        minVertex = j;
                     }
                 }
                 v[minVertex] = true;
-                mst += minCost;
+                mst += min;
                 if (cnt++ == N - 1) break;
                 for (int j = 0; j < N; j++) {
                     if (v[j]) continue;
-                    Double cost = environmentalFee(xIsland[minVertex], yIsland[minVertex], xIsland[j], yIsland[j]);
-                    if (cost < P[j]) P[j] = cost;
+                    Double cost = cost(x[j], y[j], x[minVertex], y[minVertex]);
+                    if (P[j] > cost) P[j] = cost;
                 }
             }
             System.out.println("#" + t + " " + Math.round(mst));
