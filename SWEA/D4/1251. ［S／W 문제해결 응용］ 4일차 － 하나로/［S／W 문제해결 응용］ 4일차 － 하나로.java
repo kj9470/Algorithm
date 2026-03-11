da77aa14fa90a30071186adab5969cce7931b60d
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-11 09:27:34 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 231 ms, Memory: 35,320 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,63 +2,91 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int N;
-	static double E;
-	static long[][] islands;
-	
-	static long calcDist(long[] a, long[] b) {
-		return (long) Math.pow(a[0] - b[0], 2) + (long) Math.pow(a[1] - b[1], 2);
-	}
-	
+    static int N;
+    static int[] x;
+    static int[] y;
+    static double E;
+    static int[] parents;
+
+    static class Edge {
+        int from, to;
+        double cost;
+
+        Edge(int from, int to, double cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+
+    static void makeSets() {
+        parents = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parents[i] = i;
+        }
+    }
+
+    static int findSet(int x) {
+        if (parents[x] == x) return x;
+        return parents[x] = findSet(parents[x]);
+    }
+
+    static boolean union(int x, int y) {
+        int xRoot = findSet(x);
+        int yRoot = findSet(y);
+        if (xRoot == yRoot) return false;
+        parents[yRoot] = xRoot;
+        return true;
+    }
+
+    static double getCost(int i, int j) {
+        double cx = x[i] - x[j];
+        double cy = y[i] - y[j];
+        double d = cx * cx + cy * cy;
+        return d * E;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st;
-    	
-    	int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
-        for (int t = 1; t <= T; t++) {
+        int T = Integer.parseInt(br.readLine());
+        for (int t  = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
-            islands = new long[N][2];
-            
+            x = new int[N];
+            y = new int[N];
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
-				islands[i][0] = Long.parseLong(st.nextToken());
-			}
-            
+                x[i] = Integer.parseInt(st.nextToken());
+            }
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
-				islands[i][1] = Long.parseLong(st.nextToken());
-			}
-            
+                y[i] = Integer.parseInt(st.nextToken());
+            }
             E = Double.parseDouble(br.readLine());
-        	
-            boolean[] v = new boolean[N];
-            long[] dist = new long[N]; Arrays.fill(dist, Long.MAX_VALUE);
-            dist[0] = 0;
-            long mst = 0;
-            int cnt = 0;
-            
+
+            List<Edge> edges = new ArrayList<>();
+
             for (int i = 0; i < N; i++) {
-            	long min = Long.MAX_VALUE;
-            	int minVertex = -1;
-            	for (int j = 0; j < N; j++) {
-					if(!v[j] && min > dist[j]) {
-						min = dist[j]; minVertex = j;
-					}
-				}
-            	
-            	v[minVertex] = true;
-            	mst += min;
-            	if (cnt++ == N - 1) { break; }
-            	
-            	for (int j = 0; j < N; j++) {
-					if (!v[j] && dist[j] > calcDist(islands[minVertex], islands[j])) {
-						dist[j] = calcDist(islands[minVertex], islands[j]);
-					}
-				}
-			}
-            long result = Math.round(mst * E);
-            System.out.println("#" + t + " " + result);
+                for (int j = i + 1; j < N; j++) {
+                    edges.add(new Edge(i, j, getCost(i, j)));
+                }
+            }
+
+            edges.sort(Comparator.comparingDouble(a -> a.cost));
+
+            makeSets();
+
+            double mst = 0;
+            int count = 0;
+            for (Edge e: edges) {
+                if (union(e.from, e.to)) {
+                    mst += e.cost;
+                    if (count++ == N - 1) break;
+                }
+            }
+
+            System.out.println("#" + t + " " + Math.round(mst));
         }
     }
 }
