faf1e3269749d0e3085008a31ddd728b5a30a19b
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-11 10:30:46 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 231 ms, Memory: 35,320 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -6,43 +6,11 @@ public class Solution {
     static int[] x;
     static int[] y;
     static double E;
-    static int[] parents;
 
-    static class Edge {
-        int from, to;
-        double cost;
-
-        Edge(int from, int to, double cost) {
-            this.from = from;
-            this.to = to;
-            this.cost = cost;
-        }
-    }
-
-    static void makeSets() {
-        parents = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            parents[i] = i;
-        }
-    }
-
-    static int findSet(int x) {
-        if (parents[x] == x) return x;
-        return parents[x] = findSet(parents[x]);
-    }
-
-    static boolean union(int x, int y) {
-        int xRoot = findSet(x);
-        int yRoot = findSet(y);
-        if (xRoot == yRoot) return false;
-        parents[yRoot] = xRoot;
-        return true;
-    }
-
-    static double getCost(int i, int j) {
-        double cx = x[i] - x[j];
-        double cy = y[i] - y[j];
-        double d = cx * cx + cy * cy;
+    static double cost(int x1, int y1, int x2, int y2) {
+        double x = x1 - x2;
+        double y = y1 - y2;
+        double d = (x * x) + (y * y);
         return d * E;
     }
 
@@ -53,8 +21,7 @@ public class Solution {
         int T = Integer.parseInt(br.readLine());
         for (int t  = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
-            x = new int[N];
-            y = new int[N];
+            x = new int[N]; y = new int[N];
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
                 x[i] = Integer.parseInt(st.nextToken());
@@ -65,27 +32,29 @@ public class Solution {
             }
             E = Double.parseDouble(br.readLine());
 
-            List<Edge> edges = new ArrayList<>();
-
+            boolean[] v = new boolean[N];
+            double[] P = new double[N]; Arrays.fill(P, Double.MAX_VALUE);
+            P[0] = 0.0;
+            int cnt = 0;
+            double mst = 0.0;
             for (int i = 0; i < N; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    edges.add(new Edge(i, j, getCost(i, j)));
+                int minVertex = -1;
+                double min = Double.MAX_VALUE;
+                for (int j = 0; j < N; j++) {
+                    if (!v[j] && min > P[j]) {
+                        min = P[j];
+                        minVertex = j;
+                    }
+                }
+                v[minVertex] = true;
+                mst += min;
+                if (cnt++ == N - 1) break;
+                for (int j = 0; j < N; j++) {
+                    if (v[j]) continue;
+                    double cost = cost(x[j], y[j], x[minVertex], y[minVertex]);
+                    if (P[j] > cost) P[j] = cost;
                 }
             }
-
-            edges.sort(Comparator.comparingDouble(a -> a.cost));
-
-            makeSets();
-
-            double mst = 0;
-            int count = 0;
-            for (Edge e: edges) {
-                if (union(e.from, e.to)) {
-                    mst += e.cost;
-                    if (count++ == N - 1) break;
-                }
-            }
-
             System.out.println("#" + t + " " + Math.round(mst));
         }
     }
