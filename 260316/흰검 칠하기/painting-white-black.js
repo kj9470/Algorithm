21539cc5fab1a742d95e5c4ad7a519602eb2a52e
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-16 17:38:21 +0900

    Title: 흰검 칠하기, Time: 92ms, Memory: 18MB, Status: Passed - Codetree

--- 260316/흰검 칠하기/painting-white-black.js
+++ 260316/흰검 칠하기/painting-white-black.js
@@ -1,5 +1,5 @@
-const MAX_SIZE = 200;
-const OFFSET = 100;
+const MAX_SIZE = 200001; // OFFSET * 2 + 1
+const OFFSET = 100000;  // 최대 이동 거리 1000 * 100 = 100000
 
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
@@ -7,37 +7,56 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const commands = input.slice(1).map(line => line.split(' '));
 
-let arr = Array(MAX_SIZE).fill(0);
-let current = 0;
+let white = Array(MAX_SIZE).fill(0); // L방향
+let black = Array(MAX_SIZE).fill(0); // R방향
+let color = Array(MAX_SIZE).fill(''); // 최종 색상
+let current = OFFSET;
 
 for (let command of commands) {
-    let [x, direction] = command.split(' ');
+    let [x, direction] = command;
     x = Number(x);
-    x += OFFSET;
 
     if (direction === 'R') {
-        for (let i = current; i <= x + current; i++) {
-            arr[i].push('B');
+        for (let i = current; i < current + x; i++) {
+            if (color[i] === 'G') continue;
+
+            black[i] += 1;
+
+            if (black[i] >= 2 && white[i] >= 2) {
+                color[i] = 'G'
+            } else {
+                color[i] = 'B';
+            }
         }
-    } else if (direction === 'L') {
-        for (let i = current; i >= current - x; i--) {
-            arr[i].push('W');
+        current = current + x - 1;
+    }
+    if (direction === 'L') {
+        for (let i = current; i > current - x; i--) {
+            if (color[i] === 'G') continue;
+
+            white[i] += 1;
+
+            if (black[i] >= 2 && white[i] >= 2) {
+                color[i] = 'G';
+            } else {
+                color[i] = 'W'
+            }
         }
+        current = current - x + 1;
     }
 }
 
-
+let whiteCount = 0;
+let blackCount = 0;
 let grayCount = 0;
-for (let i = 0; i < arr.length; i++) {
-    let blackCount = arr.filter((color) => color === 'B').length;
-    let whiteCount = arr.filter((color) => color === 'W').length;
 
-    if (blackCount >= 2 && whiteCount >= 2) {
+for (let i = 0; i < color.length; i++) {
+    if (color[i] === 'G') {
         grayCount += 1;
-    } else if (arr[i].includes('B')) {
-        blackCount += 1;
-    } else if (arr[i].includes('W')) {
+    } else if (color[i] === 'W') {
         whiteCount += 1;
+    } else if (color[i] === 'B') {
+        blackCount += 1;
     }
 }
 
