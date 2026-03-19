019bfb46ea6677df3e8157f7882f1ba861d62e00
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-19 10:30:46 +0900

    Title: 연속되는 수 2, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260319/연속되는 수 2/continuous-number2.js
+++ 260319/연속되는 수 2/continuous-number2.js
@@ -4,10 +4,12 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(Number);
 
-let count = 1, answer = 0;
-for (let i = 1; i < arr.length; i++) {
+let count = 0, answer = 0;
+for (let i = 0; i < arr.length; i++) {
     if (i >= 1 && arr[i] === arr[i - 1]) {
         count += 1;
+    } else {
+        count = 1;
     }
     answer = Math.max(count, answer)
 }
