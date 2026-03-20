d110fad5fa3d6e61c61adc29b1b5a553efcd2dcb
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-20 15:17:08 +0900

    [Silver V] Title: Array Rotation, Time: 140 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/28456. Array Rotation/Array Rotation.py
+++ 백준/Silver/28456. Array Rotation/Array Rotation.py
@@ -1,29 +1,38 @@
-import sys
-input=sys.stdin.readline
+def two(arr, N):
+    new_arr = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            new_arr[j][N - 1 - i] = arr[i][j]
+
+    for i in range(N):
+        for j in range(N):
+            arr[i][j] = new_arr[i][j]
+
+def one(arr, i, N):
+    tmp = arr[i][N - 1]
+    for j in range(N - 1, 0, -1):
+        arr[i][j] = arr[i][j - 1]
+    arr[i][0] = tmp
+
 
 N = int(input())
-
-lst = [[] for i in range(N)]
-
+arr = []
 for i in range(N):
-    line = list(map(int, input().split()))
-    for j in range(N):
-        lst[i].append(line[j])
-
+    arr.append(list(map(int, input().split())))
 Q = int(input())
 
 for _ in range(Q):
-    command = list(map(int, input().split()))
-    if command[0] == 1:
-        lst[command[1] - 1] = lst[command[1] - 1][-1:] + lst[command[1] - 1][:-1]
-    elif command[0] == 2:
-        lstA = [[] for i in range(N)]
-        for i in range(N - 1, -1, -1):
-            for j in range(N):
-                lstA[j].append(lst[i][j])
-        lst = lstA
+    n = list(map(int, input().split()))
+    if len(n) == 1:
+        two(arr, N)
+    else:
+        one(arr, n[1] - 1, N)
+
 
 for i in range(N):
     for j in range(N):
-        print(lst[i][j], end=" ")
+        print(arr[i][j], end=' ')
     print()
+
+
+
