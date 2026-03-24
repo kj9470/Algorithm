95435a7997a77c86140a3eeb8ab906fee141df1c
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-24 15:10:29 +0900

    Title: 벌금은 누구에게, Time: 69ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 260324/벌금은 누구에게/who-will-pay.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n, m, k] = input[0].split(" ").map(Number);
+const penalizedPersons = input.slice(1, m + 1).map(Number);
+
+const persons = Array.from({ length: n }, () => 0);
+
+let flag = false;
+let answer = 0;
+for (let penaltyNum of penalizedPersons) {
+    persons[penaltyNum] += 1;
+
+    for (let i = 0; i < n; i++) {
+        if (persons[i] >= k) {
+            flag = true;
+            answer = i;
+            break;
+        }
+    }
+}
+
+console.log(answer);
