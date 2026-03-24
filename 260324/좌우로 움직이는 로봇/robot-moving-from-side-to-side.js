9c31098b47bf333227740cc767cd5182371d302b
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-24 16:35:15 +0900

    Title: 좌우로 움직이는 로봇, Time: 302ms, Memory: 26MB, Status: Wrong Answer - Codetree

--- 260324/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
+++ 260324/좌우로 움직이는 로봇/robot-moving-from-side-to-side.js
@@ -18,7 +18,6 @@ let timeB = 1;
 for (let move of movesA) {
     let [t, d] = move.split(' ');
     t = Number(t);
-    timeA += t;
 
     for (let i = 0; i < t; i++) {
         if (d === 'R') {
@@ -27,14 +26,14 @@ for (let move of movesA) {
             currentA -= 1;
         }
 
-        posA[i] = currentA;
+        posA[timeA] = currentA;
+        timeA += 1;
     }
 }
 
 for (let move of movesB) {
     let [t, d] = move.split(' ');
     t = Number(t);
-    timeB += t;
 
     for (let i = 0; i < t; i++) {
         if (d === 'R') {
@@ -43,26 +42,26 @@ for (let move of movesB) {
             currentB -= 1;
         }
 
-        posB[i] = currentB;
+        posB[timeB] = currentB;
+        timeB += 1;
     }
 }
 
-
+// 시간이 다를 경우 부족한 쪽에서 마지막 위치로 채워줘야 함
+if (timeA < timeB) {
+    for (let i = timeA; i <= timeB; i++) {
+        posA[i] = posA[timeA];
+    }
+} else if (timeA > timeB) {
+    for (let i = timeB; i <= timeA; i++) {
+        posB[i] = posB[timeB];
+    }
+}
 
 let answer = 0;
 let totalTime = Math.max(timeA, timeB);
-
-// 시간이 다를 경우 부족한 쪽에서 마지막 위치로 채워줘야 함
-while (posA.length <= totalTime) {
-    posA.push(posA[posA.length - 1]);
-}
-
-while (posB.length <= totalTime) {
-    posB.push(posB[posB.length - 1]);
-}
-
-for (let i = 0; i < totalTime; i++) {
-    if (i >= 1 && posA[i - 1] !== posB[i - 1] && posA[i] === posB[i]) {
+for (let i = 1; i <= totalTime; i++) {
+    if (posA[i - 1] !== posB[i - 1] && posA[i] === posB[i]) {
         answer += 1;
     }
 }
