dc6844456fb11457324b598d27de174410a806b6
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-26 14:57:11 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 548 ms, Memory: 128116 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -4,20 +4,17 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb;
-
+		StringTokenizer st; StringBuilder sb;
 		st = new StringTokenizer(br.readLine());
+		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
-		List<Integer>[] G = new List[N + 1];
-		for (int i = 0; i < G.length; i++) {
-			G[i] = new ArrayList<>();
-		}
-		
 		int[] degree = new int[N + 1];
 		int[] results = new int[N + 1];
+		List<Integer>[] G = new List[N + 1];
+		for (int i = 1; i < G.length; i++) {
+			G[i] = new ArrayList<>();
+		}
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -29,16 +26,16 @@ public class Main {
 		
 		Queue<Integer> queue = new ArrayDeque<>();
 		
-		for (int i = 1; i < N + 1; i++) {
+		for (int i = 1; i < degree.length; i++) {
 			if (degree[i] == 0) {
 				queue.offer(i);
 				results[i] = 1;
 			}
 		}
 		
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			int curr = queue.poll();
-			for (int a: G[curr]) {
+			for(int a: G[curr]) {
 				degree[a]--;
 				
 				if (degree[a] == 0) {
@@ -47,9 +44,9 @@ public class Main {
 				}
 			}
 		}
-
+		
 		sb = new StringBuilder();
-		for (int i = 1; i < N + 1; i++) {
+		for (int i = 1; i < results.length; i++) {
 			sb.append(results[i]).append(" ");
 		}
 		
