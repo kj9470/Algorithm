fe8e64332709828b2ad5993dcdbe535033b14637
KwonJeong <48792069+kj9470@users.noreply.github.com>
2026-03-26 15:50:56 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 560 ms, Memory: 64744 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -2,63 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1 ,1 };
-	static int[] hdx = { -2, -1, 1, 2, -2, -1, 1, 2 };
-	static int[] hdy = { -1, -2, -2, -1, 1, 2, 2, 1 };
-	static int K, W, H;
-	static int[][] map;
+	static int[] dx = {-1, 1,  0, 0};
+	static int[] dy = { 0, 0, -1, 1};
+	static int[] hdx = {-1, -2, -2, -1,  1,  2, 2, 1};
+	static int[] hdy = {-2, -1,  1,  2, -2, -1, 1, 2};
 	
-	static int bfs() {
+ 	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st; StringBuilder sb;
+		
+		int K = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int[][] map = new int[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
 		Queue<int[]> queue = new ArrayDeque<>();
-		boolean[][][] v = new boolean[H][W][K + 1];
-		queue.offer(new int[] {0, 0, 0, K});
+		boolean[][][] v = new boolean[N][M][K + 1];
+		queue.offer(new int[] {0, 0, K, 0});
 		v[0][0][K] = true;
+		int result = -1;
 		
 		while (!queue.isEmpty()) {
 			int[] curr = queue.poll();
-			
-			if (curr[0] == H - 1 && curr[1] == W - 1) {
-				return curr[2];
+			if (curr[0] == N - 1 && curr[1] == M - 1) {
+				result = curr[3]; break;
 			}
 			for (int i = 0; i < dx.length; i++) {
-				int nx = curr[0] + dx[i], ny = curr[1] + dy[i];
-				if (0 <= nx && nx < H && 0 <= ny && ny < W && !v[nx][ny][curr[3]] && map[nx][ny] != 1) {
-					queue.offer(new int[] {nx, ny, curr[2] + 1, curr[3]});
-					v[nx][ny][curr[3]] = true;
+				int nx = curr[0] + dx[i];
+				int ny = curr[1] + dy[i];
+				if (0 <= nx && nx < N && 0 <= ny && ny < M && map[nx][ny] != 1 && !v[nx][ny][curr[2]]) {
+					queue.offer(new int[] {nx, ny, curr[2], curr[3] + 1});
+					v[nx][ny][curr[2]] = true;
 				}
 			}
-			if (curr[3] > 0) {
+			
+			if (curr[2] > 0) {
 				for (int i = 0; i < hdx.length; i++) {
-					int nx = curr[0] + hdx[i], ny = curr[1] + hdy[i];
-					if (0 <= nx && nx < H && 0 <= ny && ny < W && !v[nx][ny][curr[3] - 1] && map[nx][ny] != 1) {
-						queue.offer(new int[] {nx, ny, curr[2] + 1, curr[3] - 1});
-						v[nx][ny][curr[3] - 1] = true;
+					int nx = curr[0] + hdx[i];
+					int ny = curr[1] + hdy[i];
+					if (0 <= nx && nx < N && 0 <= ny && ny < M && map[nx][ny] != 1 && !v[nx][ny][curr[2] - 1]) {
+						queue.offer(new int[] {nx, ny, curr[2] - 1, curr[3] + 1});
+						v[nx][ny][curr[2] - 1] = true;
 					}
 				}
 			}
 		}
-		return -1;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		K = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		W = Integer.parseInt(st.nextToken());
-		H = Integer.parseInt(st.nextToken());
-		map = new int[H][W];
-		
-		for (int i = 0; i < H; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < W; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-        
-		int result = bfs();
 		
 		System.out.println(result);
 	}
